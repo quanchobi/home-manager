a374205c211b3a35646de13472973813817acb09
quanchobi <quanchobi@proton.me>
2025-10-01 21:06:50 -0400

    Fix: hm and stylix version mismatch

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
-    home-manager.url = "github:nix-community/home-manager";
+    home-manager.url = "github:nix-community/home-manager/release-25.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     stylix.url = "github:danth/stylix/release-25.05";
     zen-browser = {
